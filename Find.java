30d72a23d7f6885abad72b016346e054b85ada1b
Viktor <purple_goth@bk.ru>
2017-05-10 12:51:12 -0700

    Initial project Without PST parser yet
    05/10/2017

---
+++ Find.java
@@ -0,0 +1,92 @@
+import java.io.*;
+import java.nio.file.*;
+import java.nio.file.attribute.*;
+import static java.nio.file.FileVisitResult.*;
+import javax.swing.filechooser.FileSystemView;
+import java.util.*;
+
+public class Find {
+
+	private static int matchTotal = 0;
+	private ArrayList<Path> fileNames = new ArrayList<Path>();
+
+	public class Finder
+			extends SimpleFileVisitor<Path> {
+
+		private final PathMatcher match;
+		private int numMatches = 0;
+
+		Finder(String pattern) {
+			match = FileSystems.getDefault()
+					.getPathMatcher("glob:" + pattern);
+		}
+
+		void find(Path file) {
+			Path name = file.getFileName();
+			if (name != null && match.matches(name)) {
+				numMatches++;
+				System.out.println(file);
+				fileNames.add(file);
+			}
+		}
+
+		void done() {
+			System.out.println("Matched: "
+					+ numMatches);
+			matchTotal = matchTotal + numMatches;
+		}
+
+		@Override
+		public FileVisitResult visitFile(Path file,
+				BasicFileAttributes attrs) {
+			find(file);
+			return CONTINUE;
+		}
+
+		@Override
+		public FileVisitResult preVisitDirectory(Path dir,
+				BasicFileAttributes attrs) {
+			find(dir);
+			return CONTINUE;
+		}
+
+		@Override
+		public FileVisitResult visitFileFailed(Path file,
+				IOException exc) {
+			return CONTINUE;
+		}
+	}
+
+
+
+	public void Filesearching()
+			throws IOException {
+
+		File[] paths;
+
+		paths = File.listRoots();
+
+		for (File path : paths) {
+			String str = path.toString();
+			String slash = "\\";
+
+			String s = new StringBuilder(str).append(slash).toString();
+
+			Path startingDir = Paths.get(s);
+
+			String pattern = "*.pst";
+			
+
+			Finder finder = new Finder(pattern);
+			Files.walkFileTree(startingDir, finder);
+			finder.done();
+
+		}
+		System.out.println("Total Matched Number of Files : " + matchTotal);
+		}
+			
+	public ArrayList<Path> getNames(){
+		return fileNames;
+	}
+}
+
---
+++ Mail2Ccloud.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.*;
+import java.nio.file.*;
+import java.nio.file.attribute.*;
+
+import com.microsoft.azure.storage.*;
+import com.microsoft.azure.storage.blob.*;
+
+public class Mail2Ccloud {
+	public static final String storageConnectionString =
+		"DefaultEndpointsProtocol=https;"
+		+ "AccountName=4900acit;"
+		+ "AccountKey=OOUURGmvIirdpYkTA3P3miBph/PawGnAvIPb65C/pgBEBqSpYtHJpQAh1UEzL2v5mmpw/NKCtEz8W1+JNE/u/w==";
+
+	public static void main(String[] args) {
+		try {
+			CloudStorageAccount account = CloudStorageAccount.parse(storageConnectionString);
+            CloudBlobClient serviceClient = account.createCloudBlobClient();
+
+            // Container name must be lower case.
+            CloudBlobContainer container = serviceClient.getContainerReference("danisblop");
+            container.createIfNotExists();
+
+			ArrayList<Path> fileNames = new ArrayList<Path>();
+			
+			Find fs = new Find();
+			fs.Filesearching();
+			
+            // Upload an image file.
+			//make loop here 
+			
+			fileNames = fs.getNames();
+			
+			for (Path object: fileNames) {
+				System.out.println("file: "  + object.toString());
+				CloudBlockBlob blob = container.getBlockBlobReference(object.getFileName().toString());
+				File sourceFile = new File(object.toString());
+				blob.upload(new FileInputStream(sourceFile), sourceFile.length());				
+			}
+			
+            CloudBlockBlob blob = container.getBlockBlobReference("danisblop.jpg");
+            File sourceFile = new File("c:\\myimages\\danisblop.jpg");
+            blob.upload(new FileInputStream(sourceFile), sourceFile.length());
+
+			//downlading same file.
+            // Download the image file. uncoment if need
+			
+			//File destinationFile = new File(sourceFile.getParentFile(), "image1Download.tmp");
+			//blob.downloadToFile(destinationFile.getAbsolutePath());
+        }
+        catch (FileNotFoundException fileNotFoundException) {
+            System.out.print("FileNotFoundException encountered: ");
+            System.out.println(fileNotFoundException.getMessage());
+            System.exit(-1);
+        }
+        catch (StorageException storageException) {
+            System.out.print("StorageException encountered: ");
+            System.out.println(storageException.getMessage());
+            System.exit(-1);
+        }
+        catch (Exception e) {
+            System.out.print("Exception encountered: ");
+            System.out.println(e.getMessage());
+            System.exit(-1);
+        }
+	}
+}
